d78230568784b642ee8c1811f40e9eb0fdcd803e
julian0316 <31131461+julian0316@users.noreply.github.com>
2019-09-25 16:48:58 -0400

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace In_Class3rd
+{
+    class Employee
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Julian";
+            person.Age = 33;
+            person.HasPet = true;
+
+            person.Greeting();
+        }
+    }
+}
+
---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace In_Class3rd
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+        public void Greeting()
+        {
+            Console.WriteLine("Hello my name is " + Name + " and my age is " + Age);
+        }
+    }
+    
+    }
